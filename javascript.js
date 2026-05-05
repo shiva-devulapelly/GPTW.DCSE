b551d34ac5f789cac1d2f363b8ce9b426423b47b
shiva-devulapelly <shivadevulapelly007@gmail.com>
2026-05-05 23:57:24 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-﻿ function checkLogin() {
-        let user = document.getElementById("username").value;
-        let pass = document.getElementById("pass").value;
-    
-        if (user === "admin" && pass === "1234") {
-            return true;   
-        } else {
-            alert("Invalid Login");
-            return false;  
-        }
+﻿function checkLogin() {
+
+    let user = document.getElementById("username").value.trim().toLowerCase();
+    let pass = document.getElementById("pass").value.trim();
+
+    if (user === "admin" && pass === "1234") {
+        return true; // allow redirect
+    } else {
+        alert("Invalid Login");
+        return false; // stop form
     }
+}
